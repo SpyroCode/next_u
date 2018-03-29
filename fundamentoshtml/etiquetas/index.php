ed4025d2a56a71b27c88fbba9a71e4f43ca644a2
spyrocode <spiralnet.mx@gmail.com>
2018-03-29 16:40:09 -0600

    primera prueba de git en fundamentos de html en next_u

---
+++ fundamentoshtml/etiquetas/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,user-scalable=no">
+
+	<title>Deportes Extremos</title>
+</head>
+<body>
+	<h1>Deportes Extremos</h1>
+
+</body>
+</html>	
